f54461c55ee020614c12ae9b67e98a9759eec147
ericbcrril <ericbecerril1234@gmail.com>
2024-12-16 16:01:53 -0600

    Proyecto funcional 78%

---
+++ src/components/misc/navbar.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+//Componentes
+import { TfiEmail } from "react-icons/tfi";
+import { FaWhatsapp } from "react-icons/fa";
+//Scripts
+import sendMessage from "../../scripts/sendWaMessage";
+import sendEmail from "../../scripts/sendEmailMessage";
+
+function Navbar() {
+    const [formVisible, setVisible] = useState(false);
+    const [WaEm, sendWaEm] = useState(true);
+    const [activeSection, setActiveSection] = useState(""); // Estado para la sección activa
+
+    function handleClick(o){
+        setVisible(true)
+        sendWaEm(o)
+    }
+
+    // Detectar sección visible
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll("section");
+            let currentSection = "";
+
+            sections.forEach((section) => {
+                const rect = section.getBoundingClientRect();
+                if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+                    currentSection = section.id; // Actualiza con el ID de la sección visible
+                }
+            });
+
+            setActiveSection(currentSection); // Cambia la sección activa en el estado
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    function ContactForm() {
+        return (
+            <form
+                onSubmit={WaEm ? sendMessage : sendEmail}
+                className="contact-form"
+                style={formVisible ? { position: "fixed", display: "flex" } : { display: "none" }}
+            >
+                <label htmlFor="userName">Nombre:</label>
+                <input type="text" name="userName" id="userName" required={true} placeholder="¿Cual es tu Nombre?"/>
+                <textarea name="message" id="message" cols="30" rows="10" required={true} placeholder="Cuntanos algo sobre el proyecto que tienes en mente..."></textarea>
+                <div>
+                    <label htmlFor="wasap" style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                        <input
+                            type="radio"
+                            name="formOption"
+                            id="wasap"
+                            value="whatsapp"
+                            onClick={() => handleClick(true)}
+                        />
+                        <FaWhatsapp color="#25D366" size={20} /> WhatsApp
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="email" style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                        <input
+                            type="radio"
+                            name="formOption"
+                            id="email"
+                            value="email"
+                            onClick={() => handleClick(false)}
+                        />
+                        <TfiEmail color="#0072C6" size={20} /> Email
+                    </label>
+                </div>
+
+                <button type="submit">Enviar</button>
+            </form>
+        );
+    }
+
+    return (
+        <>
+            <nav>
+                <a href="#welcome" className={activeSection === "welcome" ? "active-link" : ""}>
+                    <img src="images/logos/BSitesLogo00.webp" alt="Logo00" />
+                </a>
+                <a href="#0" className={activeSection === "0" ? "active-link" : ""}>Bienvenido</a>
+                <a href="#1" className={activeSection === "1" ? "active-link" : ""}>Nuestro servicio</a>
+                <a href="#2" className={activeSection === "2" ? "active-link" : ""}>Paquetes</a>
+                <a href="#3" className={activeSection === "3" ? "active-link" : ""}>Politicas</a>
+                <button onClick={() => setVisible(!formVisible)}>Contactanos</button>
+            </nav>
+            <ContactForm />
+            <button 
+                onClick={() =>  window.open(`https://wa.me/+523318435194`, '_blank')} 
+                style={{display: "none"}}
+                className="btn-contactanos"
+                >Contactanos</button>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ src/scripts/sendEmailMessage.js
@@ -0,0 +1,24 @@
+const sendEmail = (event) => {
+    event.preventDefault(); // Prevenir que el formulario se envíe de la forma tradicional.
+  
+    const userName = event.target.userName.value;
+    const message = event.target.message.value;
+    const emailContact = "bsitesmanagement@gmail.com"; // Dirección de email de destino
+  
+    // Construimos el asunto y el cuerpo del correo
+    const emailSubject = `Consulta de Proyecto`;
+    const emailBody = `Hola, soy ${userName}.\nMi proyecto:\n${message}`;
+  
+    // Codificamos el contenido para que sea válido en una URL
+    const encodedSubject = encodeURIComponent(emailSubject);
+    const encodedBody = encodeURIComponent(emailBody);
+  
+    // Creamos el enlace para enviar el correo
+    const mailtoLink = `mailto:${emailContact}?subject=${encodedSubject}&body=${encodedBody}`;
+  
+    // Redirigimos al usuario a la aplicación de correo
+    window.open(mailtoLink, '_blank');
+  };
+  
+  export default sendEmail;
+  
