0d6ad26fd4b8435163837435dcbcbd35bae26494
ericbcrril <ericbecerril1234@gmail.com>
2025-03-01 19:35:51 -0600

    Renovacion de web completa

--- src/components/misc/navbar.jsx
+++ src/components/misc/navbar.jsx
@@ -1,96 +1,52 @@
 import { useState, useEffect } from "react";
-//Componentes
-import { TfiEmail } from "react-icons/tfi";
+import { motion } from "framer-motion";
 import { FaWhatsapp } from "react-icons/fa";
-//Scripts
-import sendMessage from "../../scripts/sendWaMessage";
-import sendEmail from "../../scripts/sendEmailMessage";
 
 function Navbar() {
-    const [formVisible, setVisible] = useState(false);
-    const [WaEm, setWaEm] = useState("whatsapp");
-    const [selectedIcon, setSelectedIcon] = useState(<FaWhatsapp color="#25D366" />); // Icono inicial
-    const [activeSection, setActiveSection] = useState("");
+  const [hidden, setHidden] = useState(false);
 
-    function handleSelectChange(event) {
-        const selectedValue = event.target.value;
-        setWaEm(selectedValue);
-        setSelectedIcon(selectedValue === "whatsapp" ? <FaWhatsapp color="#25D366" /> : <TfiEmail color="#0072C6" />);
-    }
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
 
-    // Detectar sección visible
-    useEffect(() => {
-        const handleScroll = () => {
-            const sections = document.querySelectorAll("section");
-            let currentSection = "";
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setHidden(true); // Oculta la navbar si el usuario baja
+      } else {
+        setHidden(false); // Muestra la navbar si el usuario sube
+      }
+      lastScrollY = window.scrollY;
+    };
 
-            sections.forEach((section) => {
-                const rect = section.getBoundingClientRect();
-                if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-                    currentSection = section.id;
-                }
-            });
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
-            setActiveSection(currentSection);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
-    function ContactForm() {
-        const handleSubmit = (event) => {
-            event.preventDefault();
-            WaEm === "whatsapp" ? sendMessage() : sendEmail();
-        };
-
-        return (
-            <form
-                onSubmit={handleSubmit}
-                className="contact-form"
-                style={formVisible ? { position: "fixed", display: "flex" } : { display: "none" }}
-            >
-                <label htmlFor="userName">Nombre:</label>
-                <input type="text" name="userName" id="userName" required placeholder="¿Cuál es tu Nombre?" />
-                
-                <textarea name="message" id="message" cols="30" rows="10" required placeholder="Cuéntanos algo sobre el proyecto que tienes en mente..."></textarea>
-                <br />
-                <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
-                    {selectedIcon} {/* Aquí se muestra el icono seleccionado */}
-                    <select id="contactMethod" name="contactMethod" value={WaEm} onChange={handleSelectChange}>
-                        <option value="whatsapp">WhatsApp</option>
-                        <option value="email">Email</option>
-                    </select>
-                </div><br />
-
-                <button type="submit">Enviar</button>
-            </form>
-        );
-    }
-
-    return (
-        <>
-            <button 
-                onClick={() => window.open(`https://wa.me/+523841177360`)} 
-                className="btn-contactanos"
-            >
-                <FaWhatsapp/>
-            </button>
-
-            <nav>
-                <a href="#Bienvenido" className={activeSection === "Bienvenido" ? "active-link" : ""}>
-                    <img src="images/logos/BSitesLogo00.webp" alt="Logo00" />
-                </a>
-                <a href="#Nosotros" className={activeSection === "Nosotros" ? "active-link" : ""}>Bienvenido</a>
-                <a href="#NuestroServicio" className={activeSection === "NuestroServicio" ? "active-link" : ""}>Nuestro servicio</a>
-                <a href="#Costos" className={activeSection === "Costos" ? "active-link" : ""}>Desarrollo web</a>
-                <a href="#Politicas" className={activeSection === "Politicas" ? "active-link" : ""}>Políticas</a>
-                <button onClick={() => setVisible(!formVisible)}>Contacto</button>
-            </nav>
-            
-            <ContactForm />
-        </>
-    );
+  return (
+    <>
+    <motion.header
+      className="navbar"
+      initial={{ y: 0 }}
+      animate={{ y: hidden ? -100 : 0 }} // Se mueve hacia arriba para ocultarse
+      transition={{ duration: 0.3, ease: "easeInOut" }}
+    >
+      <div className="navbar-content">
+        <img src="images/logos/BSitesLogo00.webp" alt="Logo00" />
+        <nav className="nav-links">
+          <ul>
+            <li><a href="#hero">Inicio</a></li>
+            <li><a href="#about">Quiénes somos</a></li>
+            <li><a href="#services">Servicios</a></li>
+            <li><a href="#pricing">Planes</a></li>
+          </ul>
+        </nav>
+      </div>
+    </motion.header>
+    <button 
+    onClick={() =>  window.open(`https://wa.me/+523841177360`)} 
+    className="btn-contactanos"
+    ><FaWhatsapp/></button>
+    </>
+  );
 }
 
 export default Navbar;
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,187 +1,301 @@
-//Inicio
-import { IoMdDownload } from "react-icons/io";
-import { useEffect } from "react";
-//Componentes
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Footer from "../components/misc/footer";
 import Navbar from "../components/misc/navbar";
-//import ScreenSizes from "../scripts/getWinScreenSizes";
 import { FaCheck } from "react-icons/fa6";
 
-function Home(){
-    const IFW = ["2807211", "7922055", "7913078"];
-    const img = IFW[Math.floor(Math.random() * IFW.length)];
-   
-    useEffect(() => {
-        const images = document.querySelectorAll(".img-secN0");
-        const images0 = document.querySelectorAll(".img-ft-desk");
-    
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("appear");
-                }
-            });
-        });
-    
-        images.forEach(image => observer.observe(image));
-        images0.forEach(image => observer.observe(image));
-    
-        return () => observer.disconnect(); // Limpia el observer al desmontar
-    }, []);
+function Home() {
+  const IFW = ["2807211", "7922055", "7913078"];
+  const img = IFW[Math.floor(Math.random() * IFW.length)];
 
-    function handleSendWaM(e){
-        let message;
-        switch(e){
-            case 0:
-                message = 'Hola me interesa desarrollar una Landing Page'
-            break;
-            case 1:
-                message = 'Hola me interesa desarrollar una Web Corporativa'
-            break;
-            case 2:
-                message = 'Hola me interesa desarrollar una Tienda en Linea'
-            break;
-            default:
-             message = 'Hola me interesa desarrollar una Landing Page'
-        }
-        let waLink = `https://wa.me/+523841177360?text=${message}`;
-        window.open(waLink)
-    }
-    
+  // Estado para determinar si el usuario está en México.
+  const [isMexico, setIsMexico] = useState(true);
 
-    return(<>
+  // Detectar la ubicación del usuario usando ipwhois
+  useEffect(() => {
+    fetch("https://ipwho.is/")
+      .then((response) => response.json())
+      .then((data) => {
+        setIsMexico(data.country_code === "MX");
+      })
+      .catch((error) => {
+        console.error("Error detectando la ubicación:", error);
+      });
+  }, []);
 
-    {/*<ScreenSizes/>*/}
+  function handleSendWaM(e) {
+    const messages = [
+      "Hola me interesa desarrollar una Landing Page",
+      "Hola me interesa desarrollar una Web Corporativa",
+      "Hola me interesa desarrollar una Tienda en Línea",
+    ];
+    let waLink = `https://wa.me/+523841177360?text=${messages[e] || messages[0]}`;
+    window.open(waLink);
+  }
 
-    <Navbar/>
+  // Definición de los planes con precio en MXN
+  const plans = [
+    {
+      title: "Landing Page",
+      priceMXN: 1499,
+      priceGlobal: 79,
+      time: 0.2,
+      features: [
+        "Diseño para Celular, Tablets y Escritorio",
+        "Hasta 3 secciones de navegación",
+        "Integración con redes sociales",
+        "Optimización básica para buscadores (SEO)",
+        "Tiempo de entrega rápido",
+      ],
+    },
+    {
+      title: "Web Corporativa",
+      priceMXN: 3499,
+      priceGlobal: 179,
+      time: 0.6,
+      features: [
+        "Diseño para Celular, Tablets y Escritorio",
+        "Hasta 5 páginas de navegación",
+        "Integración con redes sociales",
+        "Formulario de contacto funcional",
+        "Hosting gratuito por 3 meses",
+      ],
+    },
+    {
+      title: "Tienda en Línea",
+      priceMXN: 4499,
+      priceGlobal: 249,
+      time: 1,
+      features: [
+        "Diseño para Celular, Tablets y Escritorio",
+        "Sistema de productos y carrito de compras",
+        "Integración con Google Analytics",
+        "Mensajería automática a WhatsApp para pedidos",
+        "Hosting gratuito por 3 meses",
+      ],
+    },
+  ];
 
-    <section className="welcome-section" id="Bienvenido">
-        <div className="ws0">
-            <img src="images/logos/BSitesLogo01-NB.webp" alt="BSites01" />
-            <p>¡Estamos listos para transformar tu idea en una solución web de calidad!</p>
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
+        rel="stylesheet"
+      />
+
+      <Navbar />
+
+      <motion.section
+        id="hero"
+        className="hero"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <div className="hero-text">
+          <h1>
+            <strong>B-</strong>Sites
+          </h1>
+          <p>
+            ¡Estamos listos para transformar tu idea en una solución web de
+            calidad!
+          </p>
+          <a href="#about">
+            <motion.button whileHover={{ scale: 1.1 }} className="btn-hero">
+              Conoce más
+            </motion.button>
+          </a>
         </div>
-        <img src={`/images/resource/${img}.webp`} alt="WImage" className="ws1" />
-    </section>
-    
-    <main>
-        <section className="secN0" id="Nosotros">
-            <img className="img-secN0" src="/images/resource/4893415.webp" alt="" />
-                <div>
-                    <h2 className="sub-title-main">¿Quienes somos?</h2>
-                    <p>Somos una empresa mexicana de tecnología, joven, dinámica y en constante crecimiento que
-                        se dedica al desarrollo de aplicaciones y sitios web.</p>
-                    <p>Nuestro equipo está compuesto por desarrolladores apasionados por la tecnología 
-                        y enfocados en ofrecer soluciones innovadoras y eficientes al mejor precio.</p>
-                    <p>Nosotros te ayudaremos a destacar de entre los demás, diseñando y desarrollando una web
-                        funcional para tu negocio.</p>
-                </div>
-            </section>
+        <div className="hero-image">
+          <img src={`/images/resource/${img}.webp`} alt="Hero" />
+        </div>
+      </motion.section>
 
-        <section className="secN1" id="NuestroServicio">
-            <h2 className="sub-title-main">Nuestro Servicio</h2>
-                <img src="/images/resource/flujoTrabajo.webp" alt="" className="img-ft-desk"/>
-                <img src="/images/resource/flujoTrabajoMovil.webp" alt="" className="img-ft-mobile"/>
-        </section>
+      <motion.section
+        id="about"
+        className="about container"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="section-title">¿Quiénes somos?</h2>
+        <motion.div
+          className="about-content"
+          initial={{ opacity: 0, x: "-100%" }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2 }}
+        >
+          <div className="about-image">
+            <img src="/images/resource/6463428.jpg" alt="Nuestro Equipo" />
+          </div>
+          <div className="about-text">
+            <p>
+              Somos una organización de tecnología, joven, dinámica y en
+              constante crecimiento que se dedica al desarrollo de
+              aplicaciones y sitios web.
+            </p>
+            <p>
+              Nuestro equipo está compuesto por desarrolladores apasionados por
+              la tecnología y enfocados en ofrecer soluciones innovadoras y
+              eficientes al mejor precio.
+            </p>
+            <p>
+              Nosotros te ayudaremos a destacar de entre los demás, diseñando y
+              desarrollando una web funcional para tu negocio.
+            </p>
+          </div>
+        </motion.div>
+      </motion.section>
 
-        <section className="secN2" id="Costos">
-            <h2 className="sub-title-main">Costos</h2>
-            <p className="secN2-p">Obtén una aproximación del costo de desarrollo de tu proyecto</p>
-            <p className="info-text">Ofrecemos servicios de hosting accesibles y confiables para tu sitio web:</p>
+      <motion.section
+        id="services"
+        className="services container"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="section-title">Nuestros Servicios</h2>
+        <motion.div
+          className="services-cards"
+          initial={{ opacity: 0, x: "100%" }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 2 }}
+        >
+          <motion.div className="service-card" whileHover={{ scale: 1.05 }}>
+            <h3>Desarrollo Web</h3>
+            <p>
+              Creación de sitios web a medida, adaptados a tus necesidades con
+              tecnologías modernas para garantizar una experiencia rápida y
+              funcional.
+            </p>
+          </motion.div>
+          <motion.div className="service-card" whileHover={{ scale: 1.05 }}>
+            <h3>Hosting</h3>
+            <p>
+              Ofrecemos alojamiento web seguro y confiable con tiempos de carga
+              optimizados y soporte técnico para garantizar la estabilidad de tu
+              página.
+            </p>
+          </motion.div>
+          <motion.div className="service-card" whileHover={{ scale: 1.05 }}>
+            <h3>Registro de dominios</h3>
+            <p>
+              Te ayudamos a obtener el nombre de dominio ideal para tu marca,
+              asegurando una identidad digital profesional y fácil de recordar.
+            </p>
+          </motion.div>
+        </motion.div>
+        <button
+          type="button"
+          onClick={() =>
+            window.open(
+              "https://drive.google.com/file/d/1_gwtJXdj4CbdgrP34UjgXRjRekE_T1BU/view?usp=sharing"
+            )
+          }
+        >
+          Políticas
+        </button>
+      </motion.section>
 
-            <ul className="hosting-list">
-                <li><FaCheck className="check-icon" /> Hosting de 1 mes: <strong>40 MXN</strong></li>
-                <li><FaCheck className="check-icon" /> Hosting de 1 año: <strong>350 MXN</strong></li>
-                <li><FaCheck className="check-icon" /> Dominio personalizado (1 año): <strong>300 MXN</strong></li>
-            </ul>
-
-            <div className="paquetes-container">
-                {[
-                    {
-                        title: "Landing Page",
-                        price: "1500 MXN",
-                        features: [
-                            "Diseño para Celular, Tablets y Escritorio",
-                            "Hasta 3 secciones de navegación",
-                            "Integración con redes sociales",
-                            "Optimización básica para buscadores (SEO)",
-                            "Tiempo de entrega rápido"
-                        ]
-                    },
-                    {
-                        title: "Web Corporativa",
-                        price: "3500 MXN",
-                        features: [
-                            "Diseño para Celular, Tablets y Escritorio",
-                            "Hasta 5 páginas de navegación",
-                            "Integración con redes sociales",
-                            "Formulario de contacto funcional",
-                            "Hosting gratuito por 3 meses"
-                        ]
-                    },
-                    {
-                        title: "Tienda en Línea",
-                        price: "4500 MXN",
-                        features: [
-                            "Diseño para Celular, Tablets y Escritorio",
-                            "Sistema de productos y carrito de compras",
-                            "Integración con Google Analytics",
-                            "Mensajería automática a WhatsApp para pedidos",
-                            "Hosting gratuito por 3 meses"
-                        ]
-                    }
-                ].map((plan, index) => (
-                    <div className="paquete" key={index}>
-                        <div className="paquete-header">
-                            <h3>{plan.title}</h3>
-                            <h4>{plan.price}</h4>
-                        </div>
-                        <hr />
-                        <div className="paquete-features">
-                            {plan.features.map((feature, i) => (
-                                <p key={i}><FaCheck className="check-icon" /> {feature}</p>
-                            ))}
-                        </div>
-                        <button onClick={() => handleSendWaM(index)}>Elegir Plan</button>
-                        {/*<a href="http://">Mostrar ejemplo</a>*/}
-                    </div>
+      <motion.section id="pricing" className="pricing container" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }}>
+        <h2 className="section-title">Planes</h2>
+        <div className="paquetes-container">
+          {plans.map((plan, index) => (
+            <motion.div className="paquete" key={index} initial={{ scale: 0.1 }} whileInView={{ scale: 1 }} transition={{ duration: plan.time }}>
+              <div className="paquete-header">
+                <h3>{plan.title}</h3>
+                <h4>{isMexico ? `$${plan.priceMXN} MXN` : `$${plan.priceGlobal} USD`}</h4>
+              </div>
+              <hr />
+              <div className="paquete-features">
+                {plan.features.map((feature, i) => (
+                  <p key={i}><FaCheck className="check-icon" /> {feature}</p>
                 ))}
-            </div>
-        </section>
- 
-        <section className="secN3" id="Politicas">
-            <div className="politicas-container">
-                <h2 className="sub-title-main">Políticas</h2>
-                <p className="politicas-text">Para el desarrollo de una web en B-Sites</p>
+              </div>
+              <button onClick={() => handleSendWaM(index)}>Elegir Plan</button>
+            </motion.div>
+          ))}
+        </div>
+      </motion.section>
 
-                <div className="politicas-links">
-                    <a 
-                        href="/docs/Terminos y Condiciones.pdf" 
-                        download="Terminos y Condiciones B-Sites"
-                        className="download-button"
-                    >
-                        <IoMdDownload className="download-icon"/> Descargar Términos y Condiciones
-                    </a>
+      <motion.section
+        id="faq"
+        className="faq container"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="section-title">Preguntas Frecuentes</h2>
+        <p style={{ textAlign: "center" }}>
+          🔍 Click para descubrir la respuesta 🔎
+        </p>
+        <div className="faq-items">
+          <motion.details
+            className="faq-item"
+            initial={{ x: "-50%" }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            <summary>¿Cuánto tiempo tardan en entregar un proyecto?</summary>
+            <p>
+              El tiempo varía según el alcance, pero en promedio el desarrollo
+              dura entre 1 y 2 semanas.
+            </p>
+          </motion.details>
+          <motion.details
+            className="faq-item"
+            initial={{ x: "50%" }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <summary>¿Ofrecen soporte post-proyecto?</summary>
+            <p>Sí, brindamos soporte técnico durante los primeros 30 días.</p>
+          </motion.details>
+          <motion.details
+            className="faq-item"
+            initial={{ x: "-50%" }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 1.2 }}
+          >
+            <summary>¿Qué métodos de pago aceptan?</summary>
+            <p>Aceptamos transferencias bancarias y depósito.</p>
+          </motion.details>
+          <motion.details
+            className="faq-item"
+            initial={{ x: "50%" }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 1.6 }}
+          >
+            <summary>¿Puedo solicitar modificaciones al diseño?</summary>
+            <p>
+              Claro, trabajamos de forma colaborativa y estamos abiertos a
+              sugerencias para ajustar el diseño a tus necesidades.
+            </p>
+          </motion.details>
+          <motion.details
+            className="faq-item"
+            initial={{ x: "-50%" }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 1.8 }}
+          >
+            <summary>
+              ¿Qué información o materiales debo proporcionar para iniciar el
+              proyecto?
+            </summary>
+            <p>
+              Para comenzar, necesitaremos detalles sobre el alcance del
+              proyecto, contenido, imágenes, logotipos y cualquier referencia que
+              consideres importante.
+            </p>
+          </motion.details>
+        </div>
+      </motion.section>
 
-                    <span className="or-text">O</span>
-
-                    <a 
-                        href="https://drive.google.com/file/d/1_gwtJXdj4CbdgrP34UjgXRjRekE_T1BU/view?usp=sharing"
-                        className="view-link"
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        Ver en Google Drive
-                    </a>
-                </div>
-            </div>
-        </section>
-    
-        
-    </main>
-
-    <Footer/>
-
-    </>);
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
